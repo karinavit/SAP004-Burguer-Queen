212a5c71dced7ed9b10c1e8057e2ade8dceb5f81
karinavit <ka.vitangelo@gmail.com>
2020-07-27 17:42:21 -0300

    Instalação do firebase e correção do login, está funcionando, abençoa senhor

--- src/pages/Componentes/Form.js
+++ src/pages/Componentes/Form.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React from '../../../node_modules/react';
 import Input from './Input';
 import Button from './Button';
+import firebaseapp from '../fireconfig.js';
+import 'firebase/auth'
 
-/* function ActionLink() {
+/* import React from 'react';
+import Input from './Input';
+import Button from './Button' */;
+
+/* essa já era uma funcion comentada 
+function ActionLink() {
     function handleClick(e) {
       e.preventDefault();
       console.log('O link foi clicado.');
     } */
 
-function Form() { 
+/* function Form() { 
 
     return (
         <form>
@@ -19,11 +26,43 @@ function Form() {
             <Input type="password" placeholder="Senha"/>
             </p>
             <Button btname="LogIn"/>
-            {/* <a href="#" onClick={handleClick}>
-                Cadastre-se
-            </a> */}
-        </form>
-    );
-  }
-  
-  export default Form;
+                         </form>
+        );}  */
+
+      
+        
+        /* const cadastro = () => {
+          firebase.auth().createUserWithEmailAndPassword(email, pass).then(user => {
+            console.log(user)
+          }) */
+          function Form() { 
+            const [email, setEmail] = React.useState('');
+            const [pass, setPass] = React.useState('');
+            
+            const login = (e) => {
+                e.preventDefault()
+                console.log(email,pass)
+              firebaseapp.auth().signInWithEmailAndPassword(email, pass).then(user => {
+                console.log(user)
+                  })
+            };
+        
+            return (
+                <form>
+                        <p>
+                            <Input placeholder="Email" 
+                            value={email} onChange={e=> setEmail(e.target.value)}
+                            />  
+                        </p>
+                        <p>
+                            <Input type="password" placeholder="Senha" 
+                            value={pass} onChange={e=> setPass(e.target.value)}
+                            />
+                        </p>
+                    <Button onClick={e=> login(e)} btname="Logar"/>
+                </form>
+                );
+            }
+             
+                  
+          export default Form;
