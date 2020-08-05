3b20327db0a7894e5036798ed380d6e7d9d0b472
AdrianaFernando <adriana.drih@gmail.com>
2020-08-04 22:47:21 -0300

    estilizando login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from '../Componentes/Form'
 import Image from '../Componentes/ImagemFundo/ImagemFundo'
+import Logo from '../Componentes/Logo/Logo'
 import './login.css'
 
 function Login() {
@@ -11,8 +12,13 @@ function Login() {
         <div>
         <Image />
         </div>
+        <div className='logo-img'>
+            <Logo />
+        </div>
+        <div className='box'>
         <div className="form">
         <Form />   
+        </div>
         </div>
         </div>
         </>
