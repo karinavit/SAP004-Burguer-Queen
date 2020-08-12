5757113ef7aaff1fa17c678fb5b5a67a89b60381
AdrianaFernando <adriana.drih@gmail.com>
2020-08-12 18:43:07 -0300

    atualizando css pagina garcom

--- src/pages/Componentes/ProductsContainer/ProductsContainer.js
+++ src/pages/Componentes/ProductsContainer/ProductsContainer.js
@@ -132,7 +132,7 @@ function ProductsContainer() {
         <ButtonIn onClick={openAllDay}> Todo o dia </ButtonIn>
       </div>
       <div className="pr-container">
-        <div>
+        <div className='card-list'>
           {morning &&
             menu.breakfast.map((item) => (
               <div className="card" onClick={() => newRequest(item)}>
@@ -154,6 +154,40 @@ function ProductsContainer() {
               </div>
             ))}
         </div>
+        <table cellspacing='20px'>
+  <thead>      
+  <tr>
+    <th align='center'>Qtd.</th>
+    <th align='center'>Produto</th> 
+    <th align='center'>Valor</th>
+  </tr>
+  </thead>
+  <tbody>
+  {orders &&
+          orders.map((item) => (
+  <tr>
+    <td align='center'><BtnItem onClick={() => newRequest(item, 2)}>-</BtnItem>
+        {item.count}
+        <BtnItem onClick={() => newRequest(item, 1)}>+</BtnItem>
+    </td>
+    <td align='center'>{item.item}</td>
+    <td align='center'>R$ {item.price}</td>
+    <td align='center'>
+      <BtnItem onClick={() => deleteItem(item)}>X</BtnItem>
+    </td>
+  </tr>))}
+  </tbody>
+  <tfoot>
+    <tr>
+      <td colspan='4' align='right' >Total: R$ {bill},00</td>
+    </tr>
+    <tr>
+    <td colspan='4' align='center'><Button onClick={sendOrder}>Enviar Pedido</Button></td>
+    </tr>
+  </tfoot>
+</table>
+{/* <span>Total: R$ {bill},00</span>
+<Button onClick={sendOrder}>Enviar Pedido</Button> */}
       </div>
 
      {/*  <div> Comanda </div>
@@ -171,32 +205,7 @@ function ProductsContainer() {
         <span>Total: R$ {bill},00</span>
         <Button onClick={sendOrder}>Enviar Pedido</Button>
       </div> */}
-      <table>
-  <thead>      
-  <tr>
-    <th>Qtd.</th>
-    <th>Produto</th> 
-    <th>Valor</th>
-  </tr>
-  </thead>
-  <tbody>
-  {orders &&
-          orders.map((item) => (
-  <tr>
-    <td><BtnItem onClick={() => newRequest(item, 2)}>-</BtnItem>
-        <p>{item.count}</p>
-        <BtnItem onClick={() => newRequest(item, 1)}>+</BtnItem>
-    </td>
-    <td>{item.item}</td>
-    <td>R$ {item.price}</td>
-    <td>
-      <BtnItem onClick={() => deleteItem(item)}>X</BtnItem>
-    </td>
-  </tr>))}
-  </tbody>
-</table>
-<span>Total: R$ {bill},00</span>
-<Button onClick={sendOrder}>Enviar Pedido</Button>
+      
     </>
   );
 }
