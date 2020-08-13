be6cfa17af594dae9185b32ed43cd8e10b6e76ea
karinavit <ka.vitangelo@gmail.com>
2020-08-13 09:06:19 -0300

    Inserida a pros.name

--- src/pages/Componentes/BtnItem/BtnItem.js
+++ src/pages/Componentes/BtnItem/BtnItem.js
@@ -4,7 +4,7 @@ import './btnItem.css'
 function BtnItem(props) {
 
     return (
-        <button className='btn-item' onClick={props.onClick} {...props}>{props.children}</button>
+        <button className='btn-item' name={props.name} onClick={props.onClick} {...props}>{props.children}</button>
     );
   }
   
